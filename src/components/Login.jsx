943b73fa3ca36980bed28ec25f491c33a5326060
abishek17yadav <abishek17yadav@gmail.com>
2025-08-09 09:33:04 +0530

    Update Login & Signup, remove Temp, add auth.css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 import { useNavigate, Link } from 'react-router-dom'
-import './auth.css'
+import './Auth.css'
 
 function Login() {
   const [email, setEmail] = useState("")
